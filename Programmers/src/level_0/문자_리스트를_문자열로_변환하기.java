c1bb49c76876b8f536409b76f4cac7945d5433ad
labe <labe1827@gmail.com>
2024-07-13 21:44:13 +0900

    p 문자 리스트를 문자열로 변환하기 - java

---
+++ Programmers/src/level_0/문자_리스트를_문자열로_변환하기.java
@@ -0,0 +1,11 @@
+package Programmers.src.level_0;
+
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
